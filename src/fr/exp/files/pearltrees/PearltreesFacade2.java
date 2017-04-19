ec56f26116f6e16cbd45ebc6aad1c4a49ddf6c08
syncrase <pierre.taquet@gmail.com>
2017-04-19 13:42:06 +0200

    ajout de quelques commentaires pour la suite

--- src/fr/exp/files/pearltrees/PearltreesFacade2.java
+++ src/fr/exp/files/pearltrees/PearltreesFacade2.java
@@ -121,7 +121,12 @@ public class PearltreesFacade2 {
 
 		TaggedUrlDatabaseIO writer = new TaggedUrlDatabaseIO();
 
-		// TODO faire en sorte que je reçoive un PearltreesComponent!!!
+		// TODO faire en sorte que je reçoive un PearltreesComponent!!! Pas
+		// possible, la nature du PearltreesComponent est une structure de
+		// fichier => une url = un path
+		// Possible si une url apparaît plusieurs fois. problème quand il n'y a
+		// pas de tag de base!!!!
+		// Pas possible d'utiliser PearltreesComponent
 		// Permettra d'utiliser les autres méthodes de la façade + avoir à
 		// disposition tous les IO pour construire l'objet
 		ArrayList<TaggedUrl> taggedUrlList;
@@ -129,9 +134,10 @@ public class PearltreesFacade2 {
 
 		StringBuilder sb = new StringBuilder();
 		for (TaggedUrl taggedUrl : taggedUrlList) {
-			sb.append(taggedUrl.getFullTagPath() + taggedUrl.getUrl().getLabel() + "\n");
-			// sb.append("");
-			// sb.append("");
+			// TODO ici ça ne va pas. Chaque url peut avoir plusieurs folded
+			// tags
+			// Donc, récupérer le path pour chaque tag
+			sb.append(taggedUrl.getTags().get(0).getFullPath() + taggedUrl.getUrl().getLabel() + "\n");
 		}
 		return sb.toString();
 	}
